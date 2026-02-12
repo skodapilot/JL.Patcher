51fde339df0d8e9c69e28376e95bb6b2a78df713
skodapilot <anton.shirokov@osotec.com>
2026-02-12 14:28:27 +0300

    Refactor DLL scanning and patching logic; add support for SN input validation, file version checks, and backup creation.

--- JLPatcher/JLPatcher/Program.cs
+++ JLPatcher/JLPatcher/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 using Spectre.Console;
 
-const string name = "JL.patcher";
+const string name = "J-link.patcher";
 
 Console.Title = name;
 Console.ResetColor();
@@ -19,19 +20,13 @@ AnsiConsole.Write(header);
 
 var input = AnsiConsole.Prompt(
     new TextPrompt<string>("Enter SN:")
+        .DefaultValue("941000024")
         .Validate(s => int.TryParse(s, out _)
             ? ValidationResult.Success()
             : ValidationResult.Error("Only digits are allowed!"))
 );
 
-int number = int.Parse(input);
-
-var options = new EnumerationOptions
-{
-    RecurseSubdirectories = true,
-    IgnoreInaccessible = true,
-    MatchCasing = MatchCasing.CaseInsensitive
-};
+int sn = int.Parse(input);
 
 var targets = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
 {
@@ -40,32 +35,118 @@ var targets = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
 };
 
 var pending = new ConcurrentStack<string>();
-pending.Push(@"C:\");
 
-Parallel.ForEach(
-    Partitioner.Create(pending),
-    dir =>
+pending.Push(@"c:\");
+
+await AnsiConsole.Status()
+    .Spinner(Spinner.Known.Aesthetic)
+    .StartAsync("Scanning for DLL...", ctx =>
     {
-        while (pending.TryPop(out var current))
-        {
-            try
+        Parallel.ForEach(
+            Partitioner.Create(pending),
+            dir =>
             {
-                foreach (var file in Directory.EnumerateFiles(current))
+                while (pending.TryPop(out var current))
                 {
-                    if (targets.Contains(Path.GetFileName(file)))
-                        Console.WriteLine(file);
-                }
+                    try
+                    {
+                        foreach (var file in Directory.EnumerateFiles(current))
+                        {
+                            if (!targets.Contains(Path.GetFileName(file))) continue;
 
-                foreach (var sub in Directory.EnumerateDirectories(current))
-                    pending.Push(sub);
-            }
-            catch (UnauthorizedAccessException)
-            {
-                // silently skip
-            }
-            catch (PathTooLongException)
-            {
-            }
-        }
+                            var info = FileVersionInfo.GetVersionInfo(file);
+
+                            var fileInfo = new FileInfo(file);
+
+                            if (PatchDll(file))
+                            {
+                                AnsiConsole.WriteLine(
+                                    $"Patched: {fileInfo.Name} v.{info.FileVersion} in {fileInfo.Directory?.FullName}");
+                            }
+                            else
+                            {
+                                AnsiConsole.WriteLine(
+                                    $"Skipped: {fileInfo.Name} v.{info.FileVersion} in {fileInfo.Directory?.FullName}");
+                            }
+                        }
+
+                        foreach (var sub in Directory.EnumerateDirectories(current))
+                        {
+                            pending.Push(sub);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        // silently skip
+                    }
+                    catch (PathTooLongException)
+                    {
+                        // silently skip
+                    }
+                }
+            });
+        return Task.CompletedTask;
     });
 
+AnsiConsole.WriteLine(@"Done!");
+
+
+static List<int> FindAll(ReadOnlySpan<byte> buffer, int serial)
+{
+    var result = new List<int>();
+
+    var pattern = BitConverter.GetBytes(serial);
+
+    if (pattern.Length == 0 || buffer.Length < pattern.Length)
+        return result;
+
+    for (int i = 0; i <= buffer.Length - pattern.Length; i++)
+    {
+        if (buffer.Slice(i, pattern.Length).SequenceEqual(pattern))
+        {
+            result.Add(i);
+            i += pattern.Length - 1; // пропускаем перекрытия
+        }
+    }
+
+    return result;
+}
+
+bool PatchDll(string path)
+{
+    if (!File.Exists(path))
+    {
+        return false;
+    }
+
+
+    var bytes = File.ReadAllBytes(path);
+
+    var positions = FindAll(bytes, sn);
+
+    if (positions.Count == 0)
+    {
+        return false;
+    }
+
+
+    var newsn = BitConverter.GetBytes(123456);
+
+    foreach (var offset in positions)
+    {
+        Array.Copy(newsn, 0, bytes, offset, newsn.Length);
+    }
+
+    try
+    {
+        File.Copy(path, path + ".bak", true);
+        File.WriteAllBytes(path, bytes);
+        return true;
+    }
+    catch (Exception e)
+    {
+        AnsiConsole.WriteLine(e.Message);
+    }
+
+    return false;
+}
